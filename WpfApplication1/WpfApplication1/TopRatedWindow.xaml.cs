69f93648c243e9e6bd03bfc043173451c1d51afe
Allan Varkey <amvarkey@gmail.com>
2015-11-26 23:08:27 -0700

    worked on the TopRated Page

--- WpfApplication1/WpfApplication1/TopRatedWindow.xaml.cs
+++ WpfApplication1/WpfApplication1/TopRatedWindow.xaml.cs
@@ -27,9 +27,9 @@ namespace WpfApplication1
             headerSearchPanel.Children.Add(sh1);
 
             // this is for the list itme
-           ListItem recipeListItem =  new ListItem();
-            recipeListItem.recipeName.Content = "whatever";
-            trStackPanel.Children.Add(recipeListItem);
+          // ListItem recipeListItem =  new ListItem();
+            //recipeListItem.recipeName.Content = "whatever";
+          //  trStackPanel.Children.Add(recipeListItem); // this is to display in the stack panel . 
             /*
              * so this works. what you need to do is make sure that there are 5 slots for top rated recipes..so
              * you need 5 different stackpanel or grid. id go with a stack panel.
@@ -37,6 +37,33 @@ namespace WpfApplication1
              * 
              * 
              */
+            ListItem item1ListItem = new ListItem();
+            item1ListItem.recipeName.Content = "Spaghetti and Meatballs";
+            item1ListItem.time.Content = " 30 Mins";
+            item1.Children.Add(item1ListItem);
+
+            ListItem item2ListItem = new ListItem();
+            item2ListItem.recipeName.Content = "Good Ol' Beef Tacos";
+            item2ListItem.time.Content = "25 mins";
+            item2.Children.Add(item2ListItem);
+
+            ListItem item3ListItem = new ListItem();
+            item3ListItem.recipeName.Content = "Strawberry, Mango, and Yogurt Smoothie";
+            item3ListItem.time.Content = "5 Mins";
+            item3.Children.Add(item3ListItem);
+
+            ListItem item4ListItem = new ListItem();
+            item4ListItem.recipeName.Content = "Orange and Avocado Crowned Black Thai Grilled Chicken Thighs";
+            item4ListItem.time.Content = "25 Mins";
+            item4.Children.Add(item4ListItem);
+
+            ListItem item5ListItem = new ListItem();
+            item5ListItem.recipeName.Content = "Chicken, Broccoli & Pineapple Stir-Fry";
+            item5ListItem.time.Content = "30 Mins";
+            item5.Children.Add(item5ListItem);
+
+
+
 
         }// end of TopRatedWindow
     }
