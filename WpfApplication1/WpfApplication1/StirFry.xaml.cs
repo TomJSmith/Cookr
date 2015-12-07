144404df4d17fbca766596e53db8dc2c538e1d2b
TomJSmith <thomas_smith@live.ca>
2015-12-07 00:47:39 -0700

    Added serving sizes to stir fry. One more to go.

--- WpfApplication1/WpfApplication1/StirFry.xaml.cs
+++ WpfApplication1/WpfApplication1/StirFry.xaml.cs
@@ -25,6 +25,7 @@ namespace WpfApplication1
         public StirFry()
         {
             InitializeComponent();
+            comboBox.SelectedIndex = 1;
         }
 
 
@@ -74,6 +75,86 @@ namespace WpfApplication1
         public ImageSource getRating()
         {
             return image2.Source;
+        }
+
+        private void serves1(object sender, RoutedEventArgs e)
+        {
+            label4_Copy15.Content = "1/8 pound chicken breast\ncut into strips";
+            label4_Copy1.Content = "1/2 cloves of garlic\nchopped";
+            label4_Copy5.Content = "1/8 cup of soy sauce";
+            label4_Copy2.Content = "1/8 teaspoon cinnamon";
+            label4_Copy3.Content = "1/4 red bell pepper cut into\nchunks";
+            label4_Copy4.Content = "1/2 cups of canned\npineapple chunks ";
+            label4_Copy16.Content = "1/4 medium onion, sliced";
+            label4_Copy8.Content = "1/4 cup chicken broth";
+            label4_Copy9.Content = "1/4 tablespoon cornstarch";
+            label4_Copy10.Content = "1/2 cups brocolli";
+            label4_Copy11.Content = "1/2 teaspoon olive\noil";
+            label4_Copy14.Content = "1/2 teaspoon canola oil";
+        }
+
+        private void serves2(object sender, RoutedEventArgs e)
+        {
+            label4_Copy15.Content = "1/4 pound chicken breast\ncut into strips";
+            label4_Copy1.Content = "1 cloves of garlic\nchopped";
+            label4_Copy5.Content = "1/4 cup of soy sauce";
+            label4_Copy2.Content = "1/4 teaspoon cinnamon";
+            label4_Copy3.Content = "1/2 red bell pepper cut\ninto chunks";
+            label4_Copy4.Content = "1 cups of canned\npineapple chunks ";
+            label4_Copy16.Content = "1/2 medium onion, sliced";
+            label4_Copy8.Content = "1/2 cup chicken broth";
+            label4_Copy9.Content = "1/2 tablespoon cornstarch";
+            label4_Copy10.Content = "1 cups brocolli";
+            label4_Copy11.Content = "1 teaspoon olive\noil";
+            label4_Copy14.Content = "1 teaspoon canola oil";
+        }
+
+        private void serves3(object sender, RoutedEventArgs e)
+        {
+            label4_Copy15.Content = "3/8 pound chicken breast\ncut into strips";
+            label4_Copy1.Content = "1 and 1/2 cloves of\ngarlic chopped";
+            label4_Copy5.Content = "3/8 cup of soy sauce";
+            label4_Copy2.Content = "3/8 teaspoon cinnamon";
+            label4_Copy3.Content = "3/4 red bell pepper cut\ninto chunks";
+            label4_Copy4.Content = "1 and 1/2 cups of canned\npineapple chunks ";
+            label4_Copy16.Content = "3/4 medium onion, sliced";
+            label4_Copy8.Content = "3/4 cup chicken broth";
+            label4_Copy9.Content = "3/4 tablespoon cornstarch";
+            label4_Copy10.Content = "1 and 1/2 cups brocolli";
+            label4_Copy11.Content = "1 and 1/2 teaspoon\nolive oil";
+            label4_Copy14.Content = "1 and 1/2 teaspoon\ncanola oil";
+        }
+
+        private void serves4(object sender, RoutedEventArgs e)
+        {
+            label4_Copy15.Content="1/2 pound chicken breast\ncut into strips";
+            label4_Copy1.Content="2 cloves of garlic\nchopped";
+            label4_Copy5.Content="1/4 cup of soy sauce";
+            label4_Copy2.Content="1/4 teaspoon cinnamon";
+            label4_Copy3.Content="1 red bell pepper cut\ninto chunks";
+            label4_Copy4.Content="2 cups of canned\npineapple chunks ";
+            label4_Copy16.Content="1 medium onion, sliced";
+            label4_Copy8.Content="1 cup chicken broth";
+            label4_Copy9.Content="1 tablespoon cornstarch";
+            label4_Copy10.Content="2 cups brocolli";
+            label4_Copy11.Content="2 teaspoon olive oil";
+            label4_Copy14.Content="2 teaspoon canola oil";
+        }
+
+        private void serves5(object sender, RoutedEventArgs e)
+        {
+            label4_Copy15.Content = "5/8 pound chicken breast\ncut into strips";
+            label4_Copy1.Content = "2 and 1/2 cloves of\ngarlic chopped";
+            label4_Copy5.Content = "5/8 cup of soy sauce";
+            label4_Copy2.Content = "5/8 teaspoon cinnamon";
+            label4_Copy3.Content = "1 and 1/4 red bell pepper\ncut into chunks";
+            label4_Copy4.Content = "2 and 1/2 cups of canned\npineapple chunks ";
+            label4_Copy16.Content = "1 and 1/4 medium\nonion, sliced";
+            label4_Copy8.Content = "1 and 1/4 cup\nchicken broth";
+            label4_Copy9.Content = "1 and 1/4 tablespoon\ncornstarch";
+            label4_Copy10.Content = "2 and 1/2 cups brocolli";
+            label4_Copy11.Content = "2 and 1/2 teaspoon\nolive oil";
+            label4_Copy14.Content = "2 and 1/2 teaspoon\ncanola oil";
         }
     }
 }
